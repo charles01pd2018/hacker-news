2bfdf9691060beccbbb5a4703fb8dec6c1e8601c
Charlie <lucharles3@gmail.com>
2020-10-23 19:12:21 -0400

    sort

--- src/components/result/list.js
+++ src/components/result/list.js
@@ -17,11 +17,33 @@ let List = ({ list, onRemoveItem }) => {
   let sortFunction = SORTS[sort]
   let sortedList = sortFunction(list)
 
+  let sort_buttons = []
+
+  console.log(SORTS)
+  for (var sort_type in SORTS.keys) {
+    sort_buttons.push(
+      <span className='button'>
+        <button className='buttonSmall' type='button' onClick={ () => handleSort(sort_type)}>
+          {sort_type}
+        </button>
+      </span>
+    )
+  }
+  
+  console.log(sort_buttons)
+
+  
   
   return ( 
-    list.map(item => (
-    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-  )))
+
+    <div>
+      {sort_buttons}
+    {sortedList.map(item => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} /> 
+    
+  )) }
+    </div>
+  )
 }
 
 export default List
